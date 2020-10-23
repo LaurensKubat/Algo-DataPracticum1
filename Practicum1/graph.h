aa18e3d2c97d3e49d14c05b483a78da3b117c7a0
LaurensKubat <laurens.kubat@gmail.com>
2020-10-23 12:53:13 +0200

    add graph struct

---
+++ Practicum1/graph.h
@@ -0,0 +1,10 @@
+// TODO implement Graph
+
+/*
+Graph is the struct used to represent the entire graph and extra information 
+such as the number of infected, the p value and the lower and upper bound
+*/
+struct Graph
+{
+    /* data */
+};
